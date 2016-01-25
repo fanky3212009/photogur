4199c5f02b7e34c037fd8d1faa9004c0ab821159
Franky Chau <houun.chau@gmail.com>
2016-01-25 16:50:05 -0500

    Adding feature to elete pictures

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   patch 'pictures/:id' => "pictures#update"
 
 
+  delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
